7244534b79c0610fb7daeb8e861028d512c9bbf8
Edsong158 <150963218+Edsong158@users.noreply.github.com>
2024-01-25 10:38:01 -0500

    deploy

--- serve.js
+++ serve.js
@@ -6,18 +6,6 @@ const PORT = 3334;
 const app = express();
 
 
-// db.query('INSERT INTO users (username, email, password) VALUES ("jd", "jd@test.com", "password123")', (err, results) => {
-//   if (err) return console.log(err);
-
-//   console.log(results);
-// });
-
-// db.query('SELECT * FROM users', (err, results) => {
-//   if (err) return console.log(err);
-
-//   console.log(results);
-// });
-
 const api_routes = require('./routes/api_routes');
 
 // Opening up the middleware channel to allow json to be sent through from the client
@@ -41,27 +29,3 @@ app.listen(PORT, () => {
 
 
 
-
-// // GET route - Listing for the client to visit localhost:3333/test
-// app.get('/test', (requestObj, responseObj) => {
-//   responseObj.send('Hi from server!');
-// });
-
-// app.get('/', (requestObj, responseObj) => {
-//     responseObj.send('root visited');
-// });
-
-// app.get('/api/recipe', (requestObj, responseObj) => {
-//   responseObj.send({
-//     name: 'Mac & Chesse',
-//     ingredients: ['chesse', 'pasta', 'heavy cream']
-//   });
-// });
-
-// app.get('/page', (requestObj, responseObj) => {
-//     responseObj.sendFile(path.join(__dirname, './index.html'));
-// });
-
-// app.listen(PORT, () => {
-//     console.log('serve started on port', PORT);
-// });
